54974b8b28e477d18d983b97007c6cd26dcbdc8c
YeXiu2001 <raymartparadise2001@gmail.com>
2024-09-04 06:57:26 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,85 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+const img2 = require('./assets/capstone/img2.png');
+const img1 = require('./assets/capstone/img1.png');
+const img3 = require('./assets/capstone/img3.png');
+const img4 = require('./assets/capstone/img4.png');
+const img5 = require('./assets/capstone/img5.png');
+
+const images = [img2, img1, img3, img4, img5];
+
+let currentIndex = 0;
+const capsimg = document.getElementById('capsimg');
+const prevButton = document.getElementById('capsprev');
+const nextButton = document.getElementById('capsnext');
+
+function showImage(index) {
+  capsimg.src = images[index];
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+  showImage(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+  showImage(currentIndex);
+});
+
+const limg1 = require('./assets/laundry/img1.png');
+const limg2 = require('./assets/laundry/img2.png');
+const limg3 = require('./assets/laundry/img3.png');
+const limg4 = require('./assets/laundry/img4.png');
+const limg5 = require('./assets/laundry/img5.png');
+const limg6 = require('./assets/laundry/img6.png');
+
+const limages = [limg1, limg2, limg3, limg4, limg5, limg6];
+
+const limg = document.getElementById('launimg');
+const lprev = document.getElementById('lprev');
+const lnext = document.getElementById('lnext');
+
+function lshowImage(index) {
+    limg.src = limages[index];
+  }
+  
+  lprev.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    lshowImage(currentIndex);
+  });
+  
+  lnext.addEventListener('click', () => {
+    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    lshowImage(currentIndex);
+  });
+
+
+  const mimg1 = require('./assets/bwapp/bw1.png');
+    const mimg2 = require('./assets/bwapp/bw2.png');
+    const mimg3 = require('./assets/bwapp/bw3.png');
+    const mimg4 = require('./assets/bwapp/bw4.png');
+    const mimg5 = require('./assets/bwapp/bw5.png');
+    const mimg6 = require('./assets/bwapp/bw6.png');
+
+    const mimages = [mimg1, mimg2, mimg3, mimg4, mimg5, mimg6];
+
+    const mimg = document.getElementById('bwimg');
+    const mprev = document.getElementById('bwprev');
+    const mnext = document.getElementById('bwnext');
+
+    function mshowImage(index) {
+        mimg.src = mimages[index];
+      }
+
+        mprev.addEventListener('click', () => {
+            currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+            mshowImage(currentIndex);
+        });
+
+        mnext.addEventListener('click', () => {
+            currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+            mshowImage(currentIndex);
+        });
